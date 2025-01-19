13078e268e4f6960ed41dca164a262d1f706cb67
Preet Chaudhary <preetchaudhary224@gmail.com>
2025-01-19 18:38:53 +0530

    did all final things ready to upload

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from distutils.core import setup
 setup(
   name = 'TOPSIS-PREET-102203748',         # How you named your package folder (MyLib)
   packages = ['TOPSIS-PREET-102203748'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '01',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Preet',                   # Type in your name
   author_email = 'pchaudhary_be22@thapar.edu',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Preet-Chaudhary/Topsis-Preet-102203748/archive/refs/tags/01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
